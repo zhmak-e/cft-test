cb7c81561d7b2171d5b389da88f9d765f9efd92f
Yegor Zhmak <yegor.zhmak@eltex.loc>
2024-12-01 13:12:09 +0700

    SortDump

--- main.c
+++ main.c
@@ -13,7 +13,8 @@ typedef struct StatData {
 void showHelp() {
     printf("usage:\n"
         "    random <N> <filename> - writes N random records of type StatData to file\n"
-        "    load <filename> - load records of type StatData from file and print its to screen\n"
+        "    load <filename> - load records of type StatData from file and print them\n"
+        "    sort <filename> - load records from file, sort them and print\n"
         "    \n");
 }
 
@@ -28,9 +29,10 @@ void printDump(StatData *pStatData, size_t count) {
 void fillDump(StatData *pStatData, size_t count) {
     // заполняет массив p случайными данными в количестве count записей
     for(size_t i = 0; i < count; i++) {
-        pStatData[i].id = rand(); // long может принимать значения от -4 млрд до 4 млрд, но здесь просто демонстрация, поэтому будет меньше чисел (от 0 до 2 млрд)
-        pStatData[i].count = rand() % sizeof(int);
-        pStatData[i].cost = (float)rand() / 1000.0; // значения от балды
+        pStatData[i].id = rand()%10; // long может принимать значения от -4 млрд до 4 млрд, но здесь просто демонстрация, поэтому будет меньше чисел (от 0 до 2 млрд)
+        pStatData[i].count = rand() % 10;//sizeof(int);
+        // pStatData[i].cost = (float)rand() / 1000.0; // значения от балды
+        pStatData[i].cost = (rand()%20);
         pStatData[i].primary = rand() % 2;
         pStatData[i].mode = rand() % 8;
     }
@@ -78,6 +80,32 @@ int LoadDump(StatData **pStatData, size_t *count, const char* filename) {
     return 0;
 }
 
+int compareStatData(const void *pv1, const void *pv2) {
+    // сделаю сортировку по полю cost, и, если цены равны, то по полям: id, count, primary
+    // по полю mode сортировки нет
+    const StatData *p1 = pv1, *p2 = pv2;
+    return p1->cost - p2->cost ? (p1->cost - p2->cost)
+        : p1->id - p2->id ? (p1->id - p2->id)
+        : p1->count - p2->count ? (p1->count - p2->count)
+        : p1->primary - p2->primary;
+}
+
+void SortDump(StatData *pStatData, size_t count) {
+    // Напишите функцию, сортирующую два массива StatData в порядке возрастания значения поля cost.
+    // вероятно, в задании опечатка и надо сортировать лишь один массив?
+    // если надо сортировать два массива, то достаточно лишь вызвать функцию сортировки дважды - для каждого массива
+    // если надо объединить массивы вместе с сортировкой, то не описаны правила объединения
+    // к тому же, далее в тестовом сортировка используется для одного (уже объединённого) массива
+    // поэтому реализую сортировку одного массива
+
+    // воспользуюсь стандартной реализацией быстрой сортировки (если надо, можно написать свою реализацию):
+    qsort(pStatData, count, sizeof(StatData), compareStatData);
+}
+
+// JoinDump()
+// Напишите функцию, объединяющую два массива StatData Произвольной длины так, чтобы масcиве -
+// результате id всех записей был уникален.
+
 int main(int argc, char* argv[]) {
     printf("RAND_MAX=%d\n", RAND_MAX);
     printf("sizeof(StatData)=%u\n", sizeof(StatData));
@@ -117,6 +145,24 @@ int main(int argc, char* argv[]) {
             printDump(p, n);
             free(p); // в принципе, можно было бы и не освобождать, всё
                          // равно программа заканчивается, всё само подчистится
+            return 0;
+        }
+    }
+
+    if(!strcmp(argv[1], "sort")) {
+        if(argc == 3) {
+            StatData *p = NULL;
+            size_t n = 0;
+            int result = LoadDump(&p, &n, argv[2]);
+            if(result) {
+                return result;
+            }
+            printf("loaded data:\n");
+            printDump(p, n);
+            SortDump(p, n);
+            printf("after sort:\n");
+            printDump(p, n);
+            free(p);
             return 0;
         }
     }
